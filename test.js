db68d15c283ff763c09442dd85ba625f2bb5032a
chiochiwo <ccu12345@naver.com>
2021-08-11 11:08:15 +0900

    3교시 커밋

--- test.js
+++ test.js
@@ -1,4 +1,17 @@
-i=0
-document.write(i)
-
-var ar=[]; // javascript 
+let gn;
+let time;   
+function doStart() {
+    gn=5;
+    time=setInterval(showText,1000);
+    // 5부터 시작해서 1초에 한번씩 숫자가 감소해서
+    // 5, 4, 3, 2, 1, 시작. 표시라고 정지하는 코드
+    // setInterval, clearInterval 이용
+} 
+function showText() {
+    document.getElementsByName("point")[0].value=gn;
+    if(gn==0) {
+        document.getElementsByName("point")[0].value="   시작    ";
+        clearInterval(timer);
+    }
+    gn-=1;
+}   
